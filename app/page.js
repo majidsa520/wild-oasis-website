5305a68461decf68398cc7a75ff824e86b41e5a8
Majid <majidsa520@outlook.com>
2025-06-12 11:59:48 +0330

    use fake api on server component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
-import Navigation from "./components/Navigation";
-
-export default function Page() {
+export default async function Page() {
+	const users = await (
+		await fetch("https://jsonplaceholder.typicode.com/users")
+	).json();
 	return (
 		<div>
 			<h1>Welcome to wild oasis</h1>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>{`${user.username}: ${user.name}`}</li>
+				))}
+			</ul>
 		</div>
 	);
 }
