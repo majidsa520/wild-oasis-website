0918564da61983206efbb8cb5a28df63aad1b1c4
Majid <majidsa520@outlook.com>
2025-06-12 12:00:31 +0330

    clear fake api use

--- app/page.js
+++ app/page.js
@@ -1,15 +1,7 @@
-export default async function Page() {
-	const users = await (
-		await fetch("https://jsonplaceholder.typicode.com/users")
-	).json();
+export default function Page() {
 	return (
 		<div>
 			<h1>Welcome to wild oasis</h1>
-			<ul>
-				{users.map((user) => (
-					<li key={user.id}>{`${user.username}: ${user.name}`}</li>
-				))}
-			</ul>
 		</div>
 	);
 }
