a62d8095c49de603edf888896bb110e1a1161584
Majid <majidsa520@outlook.com>
2025-06-25 17:27:41 +0330

    add reservations and profile pages

--- app/account/page.js
+++ app/account/page.js
@@ -4,7 +4,9 @@ export const metadata = {
 export default function Page() {
 	return (
 		<div>
-			<h2>Account</h2>
+			<h2 className="font-semibold text-2xl text-accent-400 mb-7">
+				Welcome Majid
+			</h2>
 		</div>
 	);
 }
--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,6 @@ const josefin = Josefin_Sans({
 	subsets: ["latin"],
 	display: "swap",
 });
-console.log(josefin);
 
 export const metadata = {
 	title: {
