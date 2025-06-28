0e2c93b5a63e04088ebb6e27c0304670660e47fb
Majid <majidsa520@outlook.com>
2025-06-28 10:26:53 +0330

    add fetching and rendering cabins

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "ohxvrgskowfwbbgwjlvo.supabase.co",
+				port: "",
+				pathname: "/storage/v1/object/public/cabin-images/**",
+				search: "",
+			},
+		],
+	},
+};
+// https://ohxvrgskowfwbbgwjlvo.supabase.co/storage/v1/object/public/cabin-images/0.7254143805733573-Golden_buddha.jpg
 export default nextConfig;
