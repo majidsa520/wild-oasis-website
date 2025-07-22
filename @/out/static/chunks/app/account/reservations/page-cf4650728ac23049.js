1f6b626cbd90baf33add119d2548f62a2a270751
Majid <majidsa520@outlook.com>
2025-07-22 12:28:32 +0330

    add text expander component

--- @/out/static/chunks/app/account/reservations/page-cf4650728ac23049.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[597,160,409,555,810,404,997,346],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,3 +1,4 @@
+import TextExpander from "@/app/_components/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
@@ -12,7 +13,6 @@ export async function generateStaticParams() {
 	const ids = cabins.map((cabin) => {
 		return { cabinId: String(cabin.id) };
 	});
-	console.log(ids);
 	return ids;
 }
 
@@ -27,10 +27,10 @@ export default async function Page({ params }) {
 			<div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
 				<div className="relative scale-[1.15] -translate-x-3">
 					<Image
-						fill
 						className="object-fill"
 						src={image}
 						alt={`Cabin ${name}`}
+						fill
 					/>
 				</div>
 
@@ -39,14 +39,16 @@ export default async function Page({ params }) {
 						Cabin {name}
 					</h3>
 
-					<p className="text-lg text-primary-300 mb-10">{description}</p>
+					<p className="text-lg text-primary-300 mb-10">
+						<TextExpander>{description}</TextExpander>
+					</p>
 
 					<ul className="flex flex-col gap-4 mb-7">
 						<li className="flex gap-3 items-center">
 							<UsersIcon className="h-5 w-5 text-primary-600" />
 							<span className="text-lg">
-								For up to <span className="font-bold">{maxCapacity}</span>{" "}
-								guests
+								For up to{" "}
+								<span className="font-bold">{maxCapacity}</span> guests
 							</span>
 						</li>
 						<li className="flex gap-3 items-center">
@@ -59,7 +61,8 @@ export default async function Page({ params }) {
 						<li className="flex gap-3 items-center">
 							<EyeSlashIcon className="h-5 w-5 text-primary-600" />
 							<span className="text-lg">
-								Privacy <span className="font-bold">100%</span> guaranteed
+								Privacy <span className="font-bold">100%</span>{" "}
+								guaranteed
 							</span>
 						</li>
 					</ul>
