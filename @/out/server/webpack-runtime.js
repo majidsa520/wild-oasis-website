2834f7b96f7212d312d8f758d8e28151b5767061
Majid <majidsa520@outlook.com>
2025-07-28 10:21:03 +0330

    implement usePathname

--- @/out/server/webpack-runtime.js
+++ @/out/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d144389867578746")
+/******/ 		__webpack_require__.h = () => ("b3107e3e73e1d0c0")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
