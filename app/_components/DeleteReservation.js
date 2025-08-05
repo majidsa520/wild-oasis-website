7cfcfdaa1a8e0296478ef3b599bffaf794fdc457
Majid <majidsa520@outlook.com>
2025-08-05 11:48:29 +0330

    add delete reservation functionality

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,12 +1,32 @@
-import { TrashIcon } from '@heroicons/react/24/solid';
+"use client";
+import SpinnerMini from "@/app/_components/SpinnerMini";
+import { deleteBookingAction } from "@/app/_lib/actions";
+import { TrashIcon } from "@heroicons/react/24/solid";
+import { useTransition } from "react";
 
-function DeleteReservation({ bookingId }) {
-  return (
-    <button className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
-      <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
-      <span className='mt-1'>Delete</span>
-    </button>
-  );
+export default function DeleteReservation({ bookingId }) {
+	const [pending, setTransition] = useTransition();
+	function handleDelete() {
+		if (confirm("Are you sure about deleting this booking?"))
+			setTransition(() => deleteBookingAction(bookingId));
+	}
+	return (
+		<button
+			className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 px-3 hover:bg-accent-600 transition-colors hover:text-primary-900 my-auto w-full flex-grow"
+			onClick={handleDelete}
+			disabled={pending}
+		>
+			{!pending ? (
+				<>
+					<TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+					<span className="mt-1">Delete</span>
+				</>
+			) : (
+				<>
+					<SpinnerMini />
+					<span className="mt-1">Deleting</span>
+				</>
+			)}
+		</button>
+	);
 }
-
-export default DeleteReservation;
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,7 +1,7 @@
 "use server";
 import { auth, signIn, signOut } from "./auth";
 import { AuthError } from "next-auth";
-import { updateGuest } from "./data-service";
+import { deleteBooking, getBookings, updateGuest } from "./data-service";
 import { revalidatePath } from "next/cache";
 export async function signInAction() {
 	// we can get providers from auth url dynamically (/api/auth/providers)
@@ -37,4 +37,16 @@ export async function updateGuestAction(formData) {
 	const data = await updateGuest(guestId, dataToUpdate);
 
 	revalidatePath("/account/profile");
+}
+export async function deleteBookingAction(bookingId) {
+	const {
+		user: { guestId },
+	} = await auth();
+	const guestBookings = await getBookings(guestId);
+	const guestBookingsIds = guestBookings.map((booking) => booking.id);
+	if (!guestBookingsIds.includes(bookingId))
+		throw new Error("You don't have permission to do this.");
+	const data = await deleteBooking(bookingId);
+	revalidatePath("/account/reservation");
+	return data;
 }
