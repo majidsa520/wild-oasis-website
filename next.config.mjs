1777715677e2193219d906715699127c99aa3816
Majid <majidsa520@outlook.com>
2025-10-15 12:39:31 +0330

    fix a bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "ohxvrgskowfwbbgwjlvo.supabase.co",
-				port: "",
-				pathname: "/storage/v1/object/public/cabin-images/**",
-				search: "",
-			},
-		],
-	},
-	distDir: "@/out",
+   images: {
+      remotePatterns: [
+         {
+            protocol: "https",
+            hostname: "ohxvrgskowfwbbgwjlvo.supabase.co",
+            port: "",
+            pathname: "/storage/v1/object/public/cabin-images/**",
+            search: "",
+         },
+      ],
+   },
+   // distDir: "@/out",
 };
 // https://ohxvrgskowfwbbgwjlvo.supabase.co/storage/v1/object/public/cabin-images/0.7254143805733573-Golden_buddha.jpg
 export default nextConfig;
